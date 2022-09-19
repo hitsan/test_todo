b88a65aa33090441ca95b90c722865dfed853728
hitsan <soledewa2828@gmail.com>
2022-09-19 22:37:20 +0900

    Add comment

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
-docker-compose build
-docker-compose run --rm frontend sh -c "npm install -g create-react-app && create-react-app app --template typescript"
+#docker compose build
+docker compose run --rm frontend sh -c "npm install -g create-react-app"
+#docker compose run --rm frontend sh -c "cd app && npm install"
+#docker compose run --rm frontend sh -c "npm install -g create-react-app && create-react-app app --template typescript"
